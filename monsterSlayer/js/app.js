664538dc2096a45294a0d8f377b4c08115822c67
Jan Dahlke <janpauldahlke@gmail.com>
2018-05-11 11:43:48 +0200

    max version of monsterSlayer

--- monsterSlayer/js/app.js
+++ monsterSlayer/js/app.js
@@ -7,49 +7,89 @@ new Vue({
     playerHP: 100,
     monsterHP: 100,
     gameIsRunning: false,
+    turns : [],
   },
   methods: {
     startGame(){
       this.gameIsRunning = true;
       this.playerHP = 100;
       this.monsterHP = 100;
+      this.turns = [];
     },
     endGame(){
       this.gameIsRunning = false;
     },
     attack(){
-      //triggers attack 
-      //some logic
       //player to monster dmg
-      var max = 10; 
-      var min = 3;
-      var damage = Math.max(Math.floor(Math.random() * max), min)
+      let damage = this.calculateDamage(3,10)
+      this.turns.unshift({
+        isPlayer: true,
+        text: `The player hits monster for ${damage}`
+      })
       this.monsterHP -= damage;
-
-      if(this.monsterHP <= 0){
-        alert('You won');
-        this.gameIsRunning = false;
+      if(this.checkWin()) {
         return;
       }
-
+      this.monsterAttack()
+    },
+    monsterAttack(){
       //monster to player dmg // monster is stronk
-      max = 12;
-      min = 5;
-      damage = Math.max(Math.floor(Math.random() * max), min)
-      this.playerHP -= damage
-
-      if(this.playerHP <= 0){
-        alert('Monster won')
-        this.gameIsRunning = false;
-        return;
-      }
+      let damage =  this.calculateDamage(5,12)
+      this.turns.unshift({
+        isPlayer: false,
+        text: `The monster hits player for ${damage}`
+      })
+      this.playerHP -= damage;
+      this.checkWin();
     },
     specialAttack(){
-      //triggers SpecialAttack
-      //some logic
+      let damage = this.calculateDamage(10,20);
+      this.monsterHP -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: `The player hits monster HARD for ${damage}`
+      })
+      if(this.checkWin()) {
+        return;
+      }
+      this.monsterAttack()
     },
     heal(){
-
+      //  this.playerHP +=10; // check for overheal
+      if(this.playerHP <= 90 ){
+        this.playerHP += 10
+      }
+      else{
+        this.playerHP = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: `The player heals for 10`
+      })
+      this.monsterAttack();
+    },
+    calculateDamage(min, max){
+      return Math.max(Math.floor(Math.random() * max), min)
+    },
+    checkWin(){
+      if(this.monsterHP <= 0){
+        if(confirm('You won! new Game?')){
+          this.startGame()
+        }
+        else{
+          this.gameIsRunning = false
+        }
+        return true;
+      }else if(this.playerHP <= 0){
+        if(confirm('Monster won! New Game?')){
+          this.startGame()
+        }
+        else{
+          this.gameIsRunning = false
+        }
+        return true;
+      }
+      return false;
     }
   }
 })
